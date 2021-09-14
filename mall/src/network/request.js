b78c46e2da22502df31dcff7edbaaa511f07b3ae
unknown <1339084443@qq.com>
2021-09-14 22:00:01 +0800

    first

---
+++ mall/src/network/request.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { Toast } from "vant";
+import Loading from "../store/index";
+
+// 接口如果失效  需要找coderwhy
+const url = "http://152.136.185.210:7878/api/m5" ;
+
+let config = {
+  baseURL: url,
+  timeout: 10000
+};
+
+const _axios = axios.create(config);
+
+// 请求拦截
+_axios.interceptors.request.use(
+  req => {
+    // 当getters里面的isLoading为true再显示请求加载
+    if (Loading.getters.isLoading) {
+      Toast.loading({
+        forbidClick: true,
+        message: "加载中..."
+      });
+    }
+    return req;
+  },
+  err => {
+    return Promise.reject(err);
+  }
+);
+
+// 响应拦截
+_axios.interceptors.response.use(
+  res => {
+    Toast.clear();
+    return res.data;
+  },
+  err => {
+    Toast.clear();
+    return Promise.reject(err);
+  }
+);
+
+// 全局注册axios
+window.axios = _axios;
